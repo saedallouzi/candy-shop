69137bf0120f11fb07f566c63d10d819fb0c1414
SAED ALLOUZI <saedallouzi@wifibridge-28.home>
2021-06-03 10:29:00 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import products from "./products.js";
 
 function App() {
+  const arrayOfProducts = products.map((product) => (
+    <div>
+      <img className="candy-image" src={product.Image} />
+      <h3>{product.name}</h3>
+      <p>{product.price} JD </p>
+    </div>
+  ));
+
   return (
     <div>
       <h1 className="text">CANDY LOCO</h1>
@@ -10,6 +18,7 @@ function App() {
         className="shop-image"
         src="https://i.pinimg.com/originals/84/34/b1/8434b1295b1d746bc163d660f710a2ac.jpg"
       />
+      <div className="list">{arrayOfProducts}</div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    name: "ice-candy",
+    price: "3",
+    Image:
+      "https://www.madgetech.com/wp-content/uploads/2019/09/CandyMaking-Full.jpg",
+  },
+
+  {
+    name: "lolypop",
+    price: "5",
+    Image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/HardCandy.jpg/1200px-HardCandy.jpg",
+  },
+
+  {
+    name: "gum",
+    price: "7",
+    Image:
+      "https://images.unsplash.com/photo-1582058091505-f87a2e55a40f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FuZHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+  },
+];
+
+export default products;
